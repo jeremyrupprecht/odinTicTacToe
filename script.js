bd2aec83562a27250fbed0dfdbdac0af2cdecdfb
Jeremy <jdavid.rupprecht@gmail.com>
2023-11-29 16:55:22 -0700

    Add console-based tile placement

--- script.js
+++ script.js
@@ -1,50 +1,72 @@
+const player = (tileType) => {
+    let score = 0;
+    const getScore = () => score;
+    const incrementScore = () => score++;
+    const getTileType = () => tileType;
+    return {getScore, incrementScore, getTileType}
+}
+
+const gameBoard = (function() {
+    let gameBoard = [["", "", ""],
+                     ["", "", ""],
+                     ["", "", ""]];
+
+    const getBoard = () => gameBoard;
+
+    const placeTile = (player, x, y) => {
+        gameBoard[x][y] = `${player.getTileType()}`;
+        printBoard();
+        checkGameOver();
+    };
+    const printBoard = () => {
+        console.log(gameBoard);
+    }
+
+    const checkGameOver = () => {
+
+    };
+    const reset = () => {
+
+    };
+    return {getBoard, placeTile, printBoard, checkGameOver, reset}
+})();
+
 const controller = (function() {
-    
-    const gameBoard = (function() {
-        let gameBoard = [[]];
-        const checkGameOver = () => {
 
-        };
-        const reset = () => {
-
-        };
-        const getBoard = () => gameBoard;
-        const printBoard = () => {
-            console.log(gameBoard);
-        }
-        const placeTile = (player, x, y) => {
-            
-        };
-    })();
-    
-    const player = (tileType) => {
-        let score = 0;
-        const getScore = () => score;
-        const incrementScore = () => score++;
-        const getTileType = () => tileType;
-        return {getScore, incrementScore, getTileType}
-    }
-
-    const playRound = () => {
-        
-    }
-    
-    const play = () => {
-        // while (true) {
-
-        // }
-    }
-
-    // gameboard object already exists...
     const player1 = player("X");
     const player2 = player("O");
     let ties = 0;
-    let turn = "X";
     let gameOver = {
         over: false,
         winner: "none",
     }
+    let activePlayer = player1;
 
-    return {play}
+    const getActivePlayer = () => activePlayer;
+    const getTies = () => ties;
+    const getGameOver = () => gameOver;
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    }
+
+    const playRound = () => {
+        console.log("Round Start!");
+        let input = prompt("Enter in (zero-indexed) tile coordinates (x y):").split(" ");
+        let x = input[0];
+        let y = input[1];
+        gameBoard.placeTile(getActivePlayer(), x, y);
+        switchPlayerTurn();
+    }
+    
+    const play = () => {
+        // while (true) {
+            // while (!(gameOver.over)) {
+                 playRound();
+            // }
+        // }
+
+    }
+
+    return {play, getActivePlayer}
 })();
 controller.play();
