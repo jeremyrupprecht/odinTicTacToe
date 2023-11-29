038888731dea800337724041b927c8f45126bdbe
Jeremy <jdavid.rupprecht@gmail.com>
2023-11-29 15:52:25 -0700

    Create player fac func

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("testing...");
+const controller = (function() {
+    
+    const gameBoard = (function() {
+
+    })();
+    
+    const player = (tileType) => {
+        score = 0;
+        const getScore = () => score;
+        const incrementScore = () => score++;
+        const getTileType = () => tileType;
+        return {getScore, incrementScore, getTileType}
+    }
+
+    const playRound = () => {
+        // ....
+    }
+    
+    const play = () => {
+        // while (true) {
+
+        // }
+    }
+    
+    return {play}
+})();
+controller.play();
