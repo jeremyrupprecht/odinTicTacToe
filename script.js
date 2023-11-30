23228b23b1920afe5f3abdb8aa46b2a36191164d
Jeremy <jdavid.rupprecht@gmail.com>
2023-11-30 03:28:31 -0700

    Add win checking

--- script.js
+++ script.js
@@ -13,21 +13,64 @@ const gameBoard = (function() {
 
     const getBoard = () => gameBoard;
 
-    const placeTile = (player, x, y) => {
-        gameBoard[x][y] = `${player.getTileType()}`;
-        printBoard();
-        checkGameOver();
-    };
     const printBoard = () => {
-        console.log(gameBoard);
+        let rowText = "["
+        for(let i = 0; i < gameBoard.length; i++) {
+            for(let o = 0; o < gameBoard[i].length; o++) {
+                if (!gameBoard[i][o]) {
+                    rowText += " ,";
+                    continue;
+                }
+                rowText += gameBoard[i][o] + ",";
+            }
+            console.log(rowText + "]");
+            rowText = "[";
+        }
     }
 
-    const checkGameOver = () => {
+    const placeTile = (player, x, y) => {
+        // If there's already a tile there, don't replace it, return immediately
+        if (gameBoard[x][y]) {
+            console.log("NOPE");
+            return
+        }
+        gameBoard[x][y] = `${player.getTileType()}`;
+        printBoard();
+        checkGameOver(x, y, player);
+    };
+
+    const checkGameOver = (x, y, player) => {
+        // A win can only happen at the current move so check the current row, col
+        // and diagonal (both ways) for the a win
+        let tileType = player.getTileType();
+        let rowsNeededToWin = 0;
+        let colsNeededToWin = 0;
+        let diagsNeededToWin = 0;
+        let reverseDiagsNeededToWin = 0;
+        let boardLength = gameBoard.length;
+        for (let i = 0; i < boardLength; i++) {
+            if (gameBoard[x][i] == tileType) rowsNeededToWin++;
+            if (gameBoard[i][y] == tileType) colsNeededToWin++;
+            if (gameBoard[i][i] == tileType) diagsNeededToWin++;
+            if (gameBoard[i][boardLength - 1 - i] == tileType) reverseDiagsNeededToWin++;
+            console.log(`i ${i}`);
+            
+        }
+        console.log(reverseDiagsNeededToWin);
+        if (rowsNeededToWin == boardLength ||
+            colsNeededToWin == boardLength || 
+            diagsNeededToWin == boardLength ||
+            reverseDiagsNeededToWin == boardLength) {
+                console.log(`Player ${tileType} Wins!`);
+                player.incrementScore();
+        }
 
     };
+
     const reset = () => {
 
     };
+
     return {getBoard, placeTile, printBoard, checkGameOver, reset}
 })();
 
@@ -35,12 +78,12 @@ const controller = (function() {
 
     const player1 = player("X");
     const player2 = player("O");
+    let activePlayer = player1;
     let ties = 0;
     let gameOver = {
         over: false,
         winner: "none",
     }
-    let activePlayer = player1;
 
     const getActivePlayer = () => activePlayer;
     const getTies = () => ties;
@@ -61,12 +104,16 @@ const controller = (function() {
     const play = () => {
         // while (true) {
             // while (!(gameOver.over)) {
-                 playRound();
+                 //playRound();
             // }
         // }
 
+        for (let i = 0; i < 9; i++) {
+            playRound();
+        }
+
     }
 
-    return {play, getActivePlayer}
+    return {play, getActivePlayer, getTies}
 })();
 controller.play();
